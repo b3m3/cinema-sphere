c578bfb2a6241018dda63c216606deaf773554c3
b3m3 <dtstht@gmail.com>
2024-03-08 14:01:04 +0200

    u/HashRouter/homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 import Header from './layouts/Header/Header';
 import Footer from './layouts/Footer/Footer'
@@ -9,7 +9,7 @@ import './App.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="App">
         <Header />
         <div className="App-wrapp">
@@ -17,7 +17,7 @@ function App() {
         </div>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
